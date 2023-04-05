d3c06e24864089bf447fc4e0029c2feb78500cd4
David Justen <mail@davidjusten.de>
2023-04-05 16:17:01 +0200

    fix scripts

--- experiments/runAll.sh
+++ experiments/runAll.sh
@@ -7,26 +7,31 @@ cd experiments
 
 # Cleanup
 rm -rf experiment-results
+chmod +x 00-num-join-paths.sh
+chmod +x 01-num-intermediates.sh
+chmod +x 02-regret-budget.sh
+chmod +x 03-pipeline-performance.sh
+chmod +x 04-performance.sh
 
 # Run 00-num-join-paths
 echo "Running 00-num-join-paths..."
-sh 00-num-join-paths.sh
+./00-num-join-paths.sh
 
 # Run 01-num-intermediates
 echo "Running 01-num-intermediates..."
-sh 01-num-intermediates.sh
+./01-num-intermediates.sh
 
 # Run 02-regret-budget
 echo "Running 02-regret-budget..."
-sh 02-regret-budget.sh
+./02-regret-budget.sh
 
 # Run 02-routing-efficacy
 echo "03-pipeline-performance..."
-sh 03-pipeline-performance.sh
+./03-pipeline-performance.sh
 
 # Run 03-performance
 echo "Running 04-performance..."
-sh 04-performance.sh
+./04-performance.sh
 
 #######
 echo "Wrote results to ./experiment-results"
