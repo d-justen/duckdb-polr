f8f2b20ee64d6f17acbb33c3ac8432fcb2354e0f
David Justen <mail@davidjusten.de>
2023-06-07 14:40:55 +0200

    ..

--- experiments/installDependencies.sh
+++ experiments/installDependencies.sh
@@ -1,1 +1,12 @@
-pip3 install pandas numpy matplotlib
+#!/bin/bash
+
+VENV_PATH="$PWD/venv"
+
+if [[ ! -d "$VENV_PATH" ]]; then
+  echo "Creating Python Virtual Environment"
+  python3 -m venv $VENV_PATH
+  source "$VENV_PATH/bin/activate"
+  pip install pip --upgrade > /dev/null
+  pip -q install -r requirements.txt
+  echo "$HOSTNAME"
+fi
